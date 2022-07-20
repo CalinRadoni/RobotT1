b7dbec8aaf104dfefa2cbb7cfdc94f64c15d2d66
Calin Radoni <CalinRadoni@users.noreply.github.com>
2022-07-20 22:08:19 +0300

    Add a class for Honeywell HumidIcon I2C Humidity/Temperature Sensors

---
+++ lib/HIHSensor/HIHSensor.cpp
@@ -0,0 +1,89 @@
+#include "HIHSensor.h"
+
+#include "Wire.h"
+
+const uint16_t sensorAddr = 0x27;
+
+HIHSensor::HIHSensor(void)
+{
+    rawH = 0;
+    rawT = 0;
+}
+
+HIHSensor::~HIHSensor()
+{
+    //
+}
+
+bool HIHSensor::IsPresent(void)
+{
+    Wire.beginTransmission(sensorAddr);
+    uint8_t err = Wire.endTransmission();
+    if (err == 0) return true;
+    if (err == 2) return false;
+    log_e("HIH Sensor detection error %d at address 0x%02X", err, sensorAddr);
+    return false;
+}
+
+bool HIHSensor::ReadInit(void)
+{
+    Wire.beginTransmission(sensorAddr);
+    if (0 == Wire.endTransmission()) {
+        return true;
+    }
+    return false;
+}
+
+HIHSensor::Status HIHSensor::ReadData(void)
+{
+    const uint8_t dataLEN = 4;
+
+    uint8_t bytesReceived = Wire.requestFrom(sensorAddr, dataLEN);
+    if (dataLEN != bytesReceived) {
+        log_e("Received %d bytes instead of %d", bytesReceived, dataLEN);
+        return Status::DATA_Err;
+    }
+
+    uint8_t buffer[dataLEN];
+    Wire.readBytes(buffer, dataLEN);
+
+    uint8_t stale = buffer[0] & 0xC0;
+
+    rawH  = buffer[0] & 0x3F;
+    rawH  = rawH << 8;
+    rawH += buffer[1];
+
+    rawT  = buffer[2];
+    rawT  = rawT << 8;
+    rawT += buffer[3];
+    rawT  = rawT >> 2;
+
+    if(stale == 0x00) return Status::DATA_OK;
+    if(stale == 0x40) return Status::DATA_Stale;
+    return Status::DATA_Err;
+}
+
+uint16_t HIHSensor::GetHumidity(void)
+{
+	uint32_t val;
+
+	val = rawH;
+	val *= 5000;
+	val /= 8191;
+	val += 5;
+	val /= 10;
+	return (uint16_t)val;
+}
+
+uint16_t HIHSensor::GetTemperature(void)
+{
+	uint32_t val;
+
+	val = rawT;
+	val *= 8250;
+	val /= 8191;
+	val += 23310;
+	val += 5;
+	val /= 10;
+	return (uint16_t)val;
+}
---
+++ lib/HIHSensor/HIHSensor.h
@@ -0,0 +1,57 @@
+#ifndef HIHSensor_H
+#define HIHSensor_H
+
+#include <Arduino.h>
+
+class HIHSensor
+{
+public:
+    HIHSensor(void);
+    virtual ~HIHSensor();
+
+	enum class Status : uint8_t {
+		DATA_OK,
+		DATA_Stale,
+		DATA_Err
+	};
+
+    bool IsPresent(void);
+
+	/**
+	 * \brief Begin measurement cycle
+	 *
+	 * \details Call this function to start a measurement cycle.
+	 * The measurement cycle duration is typically 36.65 ms for temperature and humidity readings.
+	 */
+    bool ReadInit(void);
+
+	/**
+	 * \brief Read measured data
+     *
+	 * \return Status::DATA_Err for errors
+	 *         Status::DATA_OK if data is OK
+	 *         Status::DATA_Stale if data is old
+	 */
+	HIHSensor::Status ReadData(void);
+
+	/**
+	 * \brief Converts rawH to 10*humidity[%RH]
+	 *
+	 * rawH interval is [0 ... (2^14 - 2)] for [0 ... 100] %RH
+	 * output interval is [0 ... 1000]
+	 */
+	uint16_t GetHumidity(void);
+
+	/** \brief Converts rawT to 10*temperature[degK]
+	 *
+	 * rawT interval is [0 ... (2^14 - 2)] for [-40 ... +125] degC
+	 * output interval is [2331 ... 3981]. To convert in 10*degC substract 2731
+	 */
+	uint16_t GetTemperature(void);
+
+protected:
+    uint16_t rawH;
+    uint16_t rawT;
+};
+
+#endif
